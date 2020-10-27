6e9a6a03f36dfc5d90e6bb2685d47c6a936ad4c8
jiajie <jiajie.ma@thoughtworks.com>
2020-10-27 09:39:43 +0800

    [jiajie]add

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest3.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest3.java
@@ -19,7 +19,7 @@ public class LevelControllerTest3 {
     MockMvc mockMvc;
 
     @Test
-    public void should_return_basic_when_levelNumber_is_less_1() throws Exception {
+    public void should_return_advanced_when_levelNumber_is_greater_1() throws Exception {
         mockMvc.perform(get("/level"))
                 .andExpect(content().string("advanced"));
     }
